f0e798f86c041d45b5e794776fb6e41d57c73c57
Leanz652 <lean.alvarez@live.com.ar>
2022-12-13 22:27:57 -0300

    Avance finder

--- src/app/pages/home/finder/finder.component.ts
+++ src/app/pages/home/finder/finder.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-finder',
   templateUrl: './finder.component.html',
-  styleUrls: ['./finder.component.css']
+  styleUrls: ['./finder.component.css'],
 })
 export class FinderComponent {
+  activateButton() {
+    const botones = document.querySelectorAll('.boton-finder');
 
+    console.log(botones);
+  }
 }
