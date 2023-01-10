25ca2b3335850e495f8bf1ee3104fa8eb3f1f644
Leanz652 <lean.alvarez@live.com.ar>
2023-01-10 17:25:05 -0300

    Scroll RouterOutlet - BuscaTuLlanta

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-
+  scroll(id: string) {
+    const labelMarca = document.getElementById('marcaLabel');
+    labelMarca?.click();
+    const element = document.getElementById(id);
+    element?.scrollIntoView();
+  }
 }
